c90ebe0c3e18799cf1939c891f6e80e119be9e60
Tramaine Gillus <tramaine.gillus@gmail.com>
2021-08-21 16:49:11 -0400

    Send test message to SNS topic.

--- src/lambda.ts
+++ src/lambda.ts
@@ -3,16 +3,29 @@ import {
   APIGatewayProxyHandler,
   APIGatewayProxyResult,
 } from 'aws-lambda';
+import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
 import { logger } from './util/logger';
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  logger.info(event);
-  logger.info('Hello Lambda!');
+  logger.info(JSON.stringify(event));
+
+  const client = new SNSClient({
+    region: 'us-east-1',
+  });
+  const command = new PublishCommand({
+    TopicArn: process.env.SAVE_EMPLOYEE_BONUS_SNS_TOPIC,
+    Message: 'Foobar!!',
+  });
+  const response = await client.send(command);
+
+  logger.info(JSON.stringify(response));
 
   return {
     statusCode: 200,
-    body: '',
+    body: JSON.stringify({
+      message: 'Bonus pay received.',
+    }),
   };
 };
